46660bf1ab62e9d4979096cb8535a1647a2dc0f4
Ghost <culestfrosh@gmail.com>
2023-01-11 00:15:04 +0100

    users route

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const {
+  authUser,
+  registerUser,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} = require("../controllers/user");
+const {
+  protect,
+  protectMe
+ } = require('../middlewares/auth.middleware');
+
+router.get("/",protect,getUsers)
+router.post("/",(req,res,next)=>{
+  console.log("here")
+  next()
+},registerUser)
+router.post("/user/login",authUser)  
+router.get("/:id",protect,getUserById)
+router.put("/user/:id",protect,protectMe,updateUser)
+router.delete("/user/:id",protect,protectMe ,deleteUser)
+router.get("/user/:id",protect,getUserById)
+module.exports = router;
