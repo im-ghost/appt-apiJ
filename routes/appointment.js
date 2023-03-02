69fa63e68ee534c41528d0907c6906da569a262a
Ghost <culestfrosh@gmail.com>
2023-03-02 14:51:02 +0100

    created a route to check available time slots

--- routes/appointment.js
+++ routes/appointment.js
@@ -9,6 +9,10 @@ const {
   updateAppointment,
 } = require("../controllers/appointment");
 const {
+  checkAvailableDoctorSlots
+} = require("../controllers/user");
+
+const {
   protect,
   protectAppointment
  } = require('../middlewares/auth.middleware');
@@ -16,6 +20,7 @@ const {
 router.get("/user/:id",protect,getUserAppointments)
 router.get("/doctor/:id",protect,getDoctorAppointments)
 router.post("/",createAppointment)
+router.post("/slots/:id",checkAvailableDoctorSlots)
 router.put("/appointment/:id",protect,protectAppointment,updateAppointment)
 router.delete("/appointment/:id",protect,protectAppointment ,deleteAppointment)
 router.get("/appointment/:id",protect,getAppointmentById)
